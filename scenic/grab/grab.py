91b311d47358001ed1195afe4c0fa39c6fcb501d
Brokenwind <wangkun6536@163.com>
2016-12-01 11:22:25 +0800

    add the function searchScenic and searchSeniceSpiPage

--- scenic/grab/grab.py
+++ scenic/grab/grab.py
@@ -11,6 +11,7 @@ class Grab:
         # the entry point of grabing 
         self.base="http://scenic.cthy.com"
         self.provinces = DataFrame()
+        self.browser = webdriver.PhantomJS()
     def getProvinces(self):
         '''Get the information of link, area and the number of provinces.
         # Process:
@@ -19,16 +20,17 @@ class Grab:
             3): Get link,area and number information of every province
         # Return: 
             The return value is a DataFrame contains the following attributes:
-                link: the relative web address of details
-                area: the name of province
-                num:  the specified number of a province for further use
+                index:  the specified number of a province which extract from link for further use
+                link:   the relative web address of details
+                area:   the name of province
         '''
-        browser = webdriver.PhantomJS()
-        browser.get(self.base)
-        index = BeautifulSoup(browser.page_source)
-        map = index.find("map") 
+
+        self.browser.get(self.base)
+        entry = BeautifulSoup(self.browser.page_source)
+        map = entry.find("map") 
         if map:
             protemp = []
+            index = []
             # the pattern to extract number from link
             pattern = re.compile(r".*(\d\d)/")
             print ("got the the tag containing the information of provinces")
@@ -38,18 +40,61 @@ class Grab:
                 mapattrs["area"]=item.attrs["alt"]
                 number = re.findall(pattern,item.attrs["href"])
                 if number:
-                    mapattrs["num"]=number[0]
+                    index.append(number[0])
                 else:
-                    mapattrs["num"]="NULL"
+                    continue
                 protemp.append(mapattrs)
         else:
             print ("sorry,did not get the map data")
             return None
-        self.provinces = DataFrame(protemp)
+        self.provinces = DataFrame(protemp,index=index)
         return self.provinces
-        #browser.get(base+"/province-33/")
-        #temp = BeautifulSoup(browser.page_source)
-        #browser.get(base+temp.select("a[href^=/scenicSearch]")[0].attrs["href"])
+    def searchScenic(self,num):
+        """Extract scenics information of a spicified province.
+        # Parameters:
+        num: the number of a province which you want to grab scenic information
+        # Return:
+        
+        """
+        prefix = "/scenicSearch/"
+        suffix = "-0-0-0-0-1.html"
+        self.browser.get(self.base+prefix+str(num)+suffix)
+        first = BeautifulSoup(self.browser.page_source)
+        """ The content of tags:
+        # the total records
+        [<span class="f14 point">135</span>,
+        # how many pages
+        <span class="f14 point">14</span>,
+        # the number of records of one page
+        <span class="f14 point">10</span>]
+        """
+        tags = first.find(id="PagerList").select("li > span")
+        if tags:
+            pageCount = int(tags[1].string)
+            #print self.provinces.ix[str(num)]+"\ntotal: "+tags[0].string+" records.\n"+"total "+tags[1].string+" pages"
+            print "total: "+tags[0].string+" records.\n"+"total "+tags[1].string+" pages"
+        else:
+            return False
+        """
+        for i in range(1,pageCount+1):
+            searchSeniceSpiPage(str(num),str(page))
+        """
+        self.searchSeniceSpiPage(str(num),str(1))
+        return True
+
+    def searchSeniceSpiPage(self,num,page):
+        addr = "/scenicSearch/"+str(num)+"-0-0-0-0-"+str(page)+".html"
+        self.browser.get(self.base+addr)
+        page = BeautifulSoup(self.browser.page_source)
+        sightTags = page.select("div.sightlist > div.sightshow > div.sightdetail > h4 > a") 
+        if sightTags:
+            for item in sightTags:
+                print "got the link of "+item.string
+                link = item.attrs["href"]
+                print link
+        else:
+            return False
+        return True
 if __name__ == "__main__":
     grab = Grab()
-    grab.getProvinces()
+    grab.searchScenic(33)
